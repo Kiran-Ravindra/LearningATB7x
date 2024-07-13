c68de50c7de590c3d6a0465f9c29844392b08568
Kiran Ravindra <kiran.ravindra@aptean.com>
2024-07-13 10:27:39 +0530

    push 71324

---
+++ src/ex_07132024.java
@@ -0,0 +1,11 @@
+
+
+public class ex_07132024 {
+    public static void main(String[] args) {
+        boolean is_married = false;
+        boolean is_animal= true;
+
+        System.out.println(is_animal);
+        System.out.println(is_married);
+    }
+}
